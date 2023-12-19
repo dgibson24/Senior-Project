cccfe48d9c106c036efb312f3df512a6d2e42353
dgibson24 <dgibson10364@gmail.com>
2023-12-19 14:54:06 -0500

    restarting pickup scripsts

--- Assets/Scripts/Buffs/Player.cs
+++ Assets/Scripts/Buffs/Player.cs
@@ -1,64 +1,8 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-//using UnityEngine.InputSystem;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField]
-    private LayerMask pickableLayerMask;
-
-    [SerializeField]
-    private LayerMask deal;
-
-    [SerializeField]
-    private Transform playerCameraTransform;
-
-    [SerializeField]
-    private GameObject pickUpUI;
-
-    [SerializeField]
-    [Min(1)]
-    private float hitRange = 3;
-
-    //[SerializeField]
-    //private InputActionReference interactionInput;
-
-    private RaycastHit hit;
-
-    private void Start()
-    {
-        //interactionInput.action.performed += Interact;
-    }
-
     
-
-    private void Update()
-    {
-        Debug.DrawRay(playerCameraTransform.position, playerCameraTransform.forward * hitRange, Color.red);
-        if(hit.collider != null)
-        {
-            hit.collider.GetComponent<Highlight>()?.ToggleHighlight(false);
-            pickUpUI.SetActive(false);
-        }
-        if(Physics.Raycast(
-            playerCameraTransform.position, 
-            playerCameraTransform.forward, 
-            out hit, 
-            hitRange, 
-            pickableLayerMask))
-        {
-            hit.collider.GetComponent<Highlight>()?.ToggleHighlight(true);
-            pickUpUI.SetActive(true);
-        }
-    }
-
-    //private void Interact(interactionInput.CallbackContext context)
-    //{
-    //    if(hit.collider != null)
-    //    {
-    //        Debug.Log(hit.collider.name);
-    //    }
-    //}
 }
